b4d9448a89577ffa754cd5faf09683d76c5f76c3
koelnrausch <andreaslinn@gmail.com>
2022-01-22 13:44:25 +0100

    bbbb

---
+++ json-main.py
@@ -0,0 +1,51 @@
+from multiprocessing.connection import wait
+#import psutil
+import platform
+from datetime import datetime
+import socket
+import urllib.request
+import requests
+import coloredOutput
+import json
+import ligaTarget
+from datetime import datetime
+import time
+id=ligaTarget.getSerial()
+ts= datetime.now()
+
+uname=platform.uname()
+print("System:    ",uname.system)
+print("Node Name: ",uname.node)
+print("Release:   ",uname.release)
+print("Version:   ",uname.version)
+print("Machine:   ",uname.machine)
+print("Processor: ",uname.processor)
+print("\n")
+print("Hostname:  ",socket.gethostname())
+
+#params={'id':id, 'format':'json', 'tslocal':ts}
+headers={'content-type':'application/json'}
+# syslogStable("pingHostStable()","1","ID: "+id)
+baselinetime = ""
+
+while True:
+    baseurl = "https://www.openligadb.de/api/getlastchangedate/bl1/2021/20"
+    response = requests.get(baseurl, headers=headers)
+    
+    #lUp = datetime.strftime(response.text)
+    st = response.text
+    st = st.strip('"')
+    if (st != baselinetime) :
+        ts= datetime.now()
+        coloredOutput.printWarning("local time: "+ ts.strftime("%Y-%m-%d  %H:%M:%S"))
+        baselinetime = st
+        #st = "2011-11-30T09:15:55.596"
+
+        coloredOutput.printFromHost("Status: " + str(response.status_code) + "\ntext:  " + response.text)
+        lUp=datetime.strptime(st,"%Y-%m-%dT%H:%M:%S.%f").timetuple()
+        coloredOutput.printFromHost("Host changed at:    "+ st)
+    else:
+        print (".",end="")
+
+    time.sleep(2)
+
